3e607162f399f04db149fd3dd16941e24e81bd7c
Farid Alijani <farid.alijani@gmail.com>
2023-12-20 12:26:07 +0200

    check for cleaner results

--- project/settings.py
+++ project/settings.py
@@ -58,18 +58,18 @@ DATABASES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 AUTH_PASSWORD_VALIDATORS = [
-		{
-				'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-		},
-		{
-				'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-		},
-		{
-				'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-		},
-		{
-				'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-		},
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 LANGUAGE_CODE = 'en-us'
--- recsys_app/views.py
+++ recsys_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+import numpy as np
 
 digi_base_url = "https://digi.kansalliskirjasto.fi/search"
+
+def get_recsys_result(qu: str="This is a sample query phrase!"):
+	return [f"Token_{i+1}" for i in np.arange(10)]
 
 def main_page(request):
 	left_image_url = "https://www.topuniversities.com/sites/default/files/profiles/logos/tampere-university_5bbf14847d023f5bc849ec9a_large.jpg"
@@ -14,4 +18,5 @@ def main_page(request):
 		query = request.POST.get('query', '')
 		library_link = f'{digi_base_url}?query={query}'
 		context['library_link'] = library_link
+		context['recommendation_result'] = get_recsys_result(qu=query),
 	return render(request, 'recsys_app/index.html', context)

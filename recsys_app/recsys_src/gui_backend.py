3ed13a453251eec96d1ff99010846cf137976135
Farid Alijani <farid.alijani@gmail.com>
2024-02-08 00:02:08 +0200

    x20 for setting change

--- recsys_app/recsys_src/gui_backend.py
+++ recsys_app/recsys_src/gui_backend.py
@@ -21,7 +21,7 @@ HOME = os.getenv('HOME')
 USER = os.getenv('USER')
 
 lmMethod: str="stanza"
-nSPMs: int=60 # must be adjusted!
+nSPMs: int=20 # must be adjusted!
 DATASET_DIR: str = f"datasets/compressed_concatenated_SPMs"
 compressed_spm_file = os.path.join(HOME, DATASET_DIR, f"concat_x{nSPMs}.tar.gz")
 spm_files_dir = os.path.join(HOME, DATASET_DIR, f"concat_x{nSPMs}")

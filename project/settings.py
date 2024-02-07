6ba774036bc5c3f1e1084bed7b266c85c3112820
Farid Alijani <farid.alijani@gmail.com>
2024-02-07 19:52:23 +0200

    tokenizer inp cleaner static

--- project/settings.py
+++ project/settings.py
@@ -87,4 +87,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) # Extra places for collectstatic to find static files
 INTERNAL_IPS = ['127.0.0.1']

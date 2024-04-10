9f8e8c5e0636bc87ef5cbad7cdf26e1b74062373
Farid Alijani <farid.alijani@gmail.com>
2024-04-10 18:40:04 +0300

    x115 rahti K=250

--- recsys_app/recsys_src/gui_backend.py
+++ recsys_app/recsys_src/gui_backend.py
@@ -182,7 +182,7 @@ def get_recsys_results(query_phrase: str="This is a sample query phrase!", nToke
 		raw_query=query_phrase,
 		tok_query=tokenized_query_phrase,
 		meaningless_lemmas_list=UNQ_STW,
-		K=50,
+		K=250,
 	)
 	# print(f">>> Found {len(topKtokens)} Recommendations...")
 	return topKtokens
